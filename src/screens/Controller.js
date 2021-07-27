25a01b75ca2280459bf66c531d06d01a620d54d6
abhishek <abhi.devwork@outlook.com>
2021-07-27 22:37:05 +0530

    Component design

---
+++ src/screens/Controller.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Controller = () => {
+    return(
+        <div>
+            <h1>h</h1>
+        </div>
+    )
+};
+export default Controller;
---
+++ src/screens/home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../header/Header";
+import Movies from "../movies/Movies";
+import Showcase from "../showcase/Showcase";
+import Details from "../moviedetails/Details";
+
+
+const Homepage = () => {
+
+    return(
+        <div>
+            <Header/>
+            <Showcase/>
+            <Movies/>
+            <Details/>
+        </div>
+    )
+
+}
+
+export default Homepage;
