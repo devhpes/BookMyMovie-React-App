747082197d14bb6498bd6cd91140160e8418c0f7
abhishek <abhi.devwork@outlook.com>
2021-07-28 01:58:27 +0530

    Modal not open while clicking on login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Header.css";
+import "../header/Header.css";
 import Button from "@material-ui/core/Button";
 import logo from "../../assets/logo.svg";
 import Modal from "react-modal";
@@ -14,16 +14,19 @@ import CardContent from "@material-ui/core/CardContent";
 import { withStyles } from "@material-ui/core/styles";
 import DialogContent from "@material-ui/core/DialogContent";
 
-const styles = (theme) => ({
-  Container: {
-    display: "flex",
-    flexWrap: "wrap",
-    position: "absolute",
+const customStyles = {
+  content: {
     top: "50%",
     left: "50%",
-    transform: "translate(-50%,-50%)",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
     textAlign: "center",
   },
+};
+
+const styles = (theme) => ({
   Input: {
     margin: theme.spacing.unit,
     padding: theme.spacing.unit,
@@ -33,20 +36,30 @@ const styles = (theme) => ({
   },
   inputLable: {
     marginLeft: "8px",
-    marginTop: "12px",
+    marginTop: "15px",
   },
 });
+
+Modal.setAppElement(document.getElementById("root"));
 
 const Header = (props) => {
   const { classes } = props;
 
   const [value, setValue] = useState(0);
 
-  const [open, setOpen] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -69,6 +82,14 @@ const Header = (props) => {
     setValue({ newValue });
   };
 
+  const usernameChangeHandler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
   const loginHandler = () => {
     const encodeUsernameAndPassword = window.btoa(`${username}:${password}`);
     console.log(encodeUsernameAndPassword);
@@ -84,7 +105,7 @@ const Header = (props) => {
       .then((res) => res.json())
       .then(
         (response) => {
-          setLoggedIn(response)
+          setLoggedIn(response);
         },
         (error) => {
           setError(error);
@@ -92,16 +113,7 @@ const Header = (props) => {
       );
   };
 
-  const usernameChangeHandler = (e) => {
-    setUsername({ username: e.target.value });
-  };
-
-  const passwordChangeHandler = (e) => {
-    setPassword({ password: e.target.value });
-  };
-
   const registerHandler = () => {
-
     fetch("http://localhost:8085/api/v1/signup", {
       method: "POST",
       headers: {
@@ -133,23 +145,23 @@ const Header = (props) => {
   }, []);
 
   const firstNameChangeHandler = (e) => {
-    setFirstName({ firstname: e.target.value });
+    setFirstName(e.target.value);
   };
 
   const lastNameChangeHandler = (e) => {
-    setLastName({ lastname: e.target.value });
+    setLastName(e.target.value);
   };
 
   const emailChangeHandler = (e) => {
-    setEmail({ email: e.target.value });
+    setEmail(e.target.value);
   };
 
   const regPasswordChangeHandler = (e) => {
-    setRegPassword({ regPassword: e.target.value });
+    setRegPassword(e.target.value);
   };
 
   const contactChangeHandler = (e) => {
-    setContact({ contact: e.target.value });
+    setContact(e.target.value);
   };
 
   const logoutHandler = (e) => {
@@ -166,7 +178,7 @@ const Header = (props) => {
       <div className="topnav">
         <img src={logo} className="logo" alt="Movies App Logo" />
         <div className="topnav-right">
-          <Button variant="contained" color="default" onClick={handleOpen}>
+          <Button variant="contained" color="default" onClick={openModal}>
             Login
           </Button>
         </div>
@@ -179,206 +191,195 @@ const Header = (props) => {
       </div>
 
       <Modal
-       open={open}
-        ariaHideApp={false}
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
         contentLabel="Login"
-        className={classes.Container}
         aria-labelledby="Modal"
         aria-describedby="Modal for Login and Registration"
+        style={customStyles}
       >
-        <DialogContent>
-          <Paper>
-            <CardContent>
-              <Tabs value={value} onChange={handleTabChange} centered>
-                <Tab label="Login" />
-                <Tab label="Register" />
-              </Tabs>
+        <Paper>
+          <CardContent>
+            <Tabs value={value} onChange={handleTabChange} centered>
+              <Tab label="Login" />
+              <Tab label="Register" />
+            </Tabs>
+            {value === 0 && (
+              <div>
+                <FormControl required>
+                  <InputLabel htmlFor="username" className={classes.inputLable}>
+                    Username
+                  </InputLabel>
+                  <Input
+                    className={classes.Input}
+                    id="username"
+                    type="text"
+                    username={username}
+                    onChange={usernameChangeHandler}
+                  />
+                  <FormHelperText>
+                    <span className="red">required</span>
+                  </FormHelperText>
+                </FormControl>
 
-              {value === 0 && (
-                <div>
-                  <FormControl required>
-                    <InputLabel
-                      htmlFor="username"
-                      className={classes.inputLable}
-                    >
-                      Username
-                    </InputLabel>
-                    <Input
-                      className={classes.Input}
-                      id="username"
-                      type="text"
-                      username={username}
-                      onChange={usernameChangeHandler}
-                    />
-                    <FormHelperText>
-                      <span className="red">required</span>
-                    </FormHelperText>
-                  </FormControl>
+                <br />
+                <br />
 
-                  <br />
-                  <br />
-
-                  <FormControl required>
-                    <InputLabel
-                      htmlFor="loginPassword"
-                      className={classes.inputLable}
-                    >
-                      Password
-                    </InputLabel>
-                    <Input
-                      className={classes.Input}
-                      id="loginPassword"
-                      type="password"
-                      regPassword={regPassword}
-                      onChange={passwordChangeHandler}
-                    />
-                    <FormHelperText>
-                      <span className="red">required</span>
-                    </FormHelperText>
-                  </FormControl>
-                  <br />
-                  <br />
-                  {loggedIn === true && (
-                    <FormControl>
-                      <span className="success-text">Login Successful!</span>
-                    </FormControl>
-                  )}
-
-                  <br />
-                  <br />
-
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={loginHandler}
+                <FormControl required>
+                  <InputLabel
+                    htmlFor="loginPassword"
+                    className={classes.inputLable}
                   >
-                    LOGIN
-                  </Button>
-                </div>
-              )}
+                    Password
+                  </InputLabel>
+                  <Input
+                    className={classes.Input}
+                    id="loginPassword"
+                    type="password"
+                    password={password}
+                    onChange={passwordChangeHandler}
+                  />
+                  <FormHelperText>
+                    <span className="red">required</span>
+                  </FormHelperText>
+                </FormControl>
+                <br />
+                <br />
+                {loggedIn === true && (
+                  <FormControl>
+                    <span className="success-text">Login Successful!</span>
+                  </FormControl>
+                )}
 
-              {value === 1 && (
-                <div>
-                  <FormControl required>
-                    <InputLabel
-                      htmlFor="firstname"
-                      className={classes.inputLable}
-                    >
-                      First Name
-                    </InputLabel>
-                    <Input
-                      className={classes.Input}
-                      id="firstname"
-                      type="text"
-                      firstname={firstname}
-                      onChange={firstNameChangeHandler}
-                    />
-                    <FormHelperText>
-                      <span className="red">required</span>
-                    </FormHelperText>
-                  </FormControl>
+                <br />
+                <br />
 
-                  <br />
-                  <br />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={loginHandler}
+                >
+                  LOGIN
+                </Button>
+              </div>
+            )}
 
-                  <FormControl required>
-                    <InputLabel
-                      htmlFor="lastname"
-                      className={classes.inputLable}
-                    >
-                      Last Name
-                    </InputLabel>
-                    <Input
-                      className={classes.Input}
-                      id="lastname"
-                      type="text"
-                      lastname={lastname}
-                      onChange={lastNameChangeHandler}
-                    />
-                    <FormHelperText>
-                      <span className="red">required</span>
-                    </FormHelperText>
-                  </FormControl>
-                  <br />
-                  <br />
-                  <FormControl required>
-                    <InputLabel htmlFor="email" className={classes.inputLable}>
-                      Email
-                    </InputLabel>
-                    <Input
-                      className={classes.Input}
-                      id="email"
-                      type="text"
-                      email={email}
-                      onChange={emailChangeHandler}
-                    />
-                    <FormHelperText>
-                      <span className="red">required</span>
-                    </FormHelperText>
-                  </FormControl>
-                  <br />
-                  <br />
-                  <FormControl required>
-                    <InputLabel
-                      htmlFor="registerPassword"
-                      className={classes.inputLable}
-                    >
-                      Password
-                    </InputLabel>
-                    <Input
-                      className={classes.Input}
-                      id="registerPassword"
-                      type="password"
-                      regpassword={regPassword}
-                      onChange={regPasswordChangeHandler}
-                    />
-                    <FormHelperText>
-                      <span className="red">required</span>
-                    </FormHelperText>
-                  </FormControl>
-                  <br />
-                  <br />
-                  <FormControl required>
-                    <InputLabel
-                      htmlFor="contact"
-                      className={classes.inputLable}
-                    >
-                      Contact No.
-                    </InputLabel>
-                    <Input
-                      className={classes.Input}
-                      id="contact"
-                      type="text"
-                      contact={contact}
-                      onChange={contactChangeHandler}
-                    />
-                    <FormHelperText>
-                      <span className="red">required</span>
-                    </FormHelperText>
-                  </FormControl>
-                  <br />
-                  <br />
-                  {registrationSucces === true && (
-                    <FormControl>
-                      <span className="success">
-                        Registration Successful. Please Login!
-                      </span>
-                    </FormControl>
-                  )}
-                  <br />
-                  <br />
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={registerHandler}
+            {value === 1 && (
+              <div>
+                <FormControl required>
+                  <InputLabel
+                    htmlFor="firstname"
+                    className={classes.inputLable}
                   >
-                    REGISTER
-                  </Button>
-                </div>
-              )}
-            </CardContent>
-          </Paper>
-        </DialogContent>
+                    First Name
+                  </InputLabel>
+                  <Input
+                    className={classes.Input}
+                    id="firstname"
+                    type="text"
+                    firstname={firstname}
+                    onChange={firstNameChangeHandler}
+                  />
+                  <FormHelperText>
+                    <span className="red">required</span>
+                  </FormHelperText>
+                </FormControl>
+
+                <br />
+                <br />
+
+                <FormControl required>
+                  <InputLabel htmlFor="lastname" className={classes.inputLable}>
+                    Last Name
+                  </InputLabel>
+                  <Input
+                    className={classes.Input}
+                    id="lastname"
+                    type="text"
+                    lastname={lastname}
+                    onChange={lastNameChangeHandler}
+                  />
+                  <FormHelperText>
+                    <span className="red">required</span>
+                  </FormHelperText>
+                </FormControl>
+                <br />
+                <br />
+                <FormControl required>
+                  <InputLabel htmlFor="email" className={classes.inputLable}>
+                    Email
+                  </InputLabel>
+                  <Input
+                    className={classes.Input}
+                    id="email"
+                    type="text"
+                    email={email}
+                    onChange={emailChangeHandler}
+                  />
+                  <FormHelperText>
+                    <span className="red">required</span>
+                  </FormHelperText>
+                </FormControl>
+                <br />
+                <br />
+                <FormControl required>
+                  <InputLabel
+                    htmlFor="registerPassword"
+                    className={classes.inputLable}
+                  >
+                    Password
+                  </InputLabel>
+                  <Input
+                    className={classes.Input}
+                    id="registerPassword"
+                    type="password"
+                    regPassword={regPassword}
+                    onChange={regPasswordChangeHandler}
+                  />
+                  <FormHelperText>
+                    <span className="red">required</span>
+                  </FormHelperText>
+                </FormControl>
+                <br />
+                <br />
+                <FormControl required>
+                  <InputLabel htmlFor="contact" className={classes.inputLable}>
+                    Contact No.
+                  </InputLabel>
+                  <Input
+                    className={classes.Input}
+                    id="contact"
+                    type="text"
+                    contact={contact}
+                    onChange={contactChangeHandler}
+                  />
+                  <FormHelperText>
+                    <span className="red">required</span>
+                  </FormHelperText>
+                </FormControl>
+                <br />
+                <br />
+                {registrationSucces === true && (
+                  <FormControl>
+                    <span className="success">
+                      Registration Successful. Please Login!
+                    </span>
+                  </FormControl>
+                )}
+                <br />
+                <br />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={registerHandler}
+                >
+                  REGISTER
+                </Button>
+              </div>
+            )}
+          </CardContent>
+        </Paper>
       </Modal>
     </div>
   );
